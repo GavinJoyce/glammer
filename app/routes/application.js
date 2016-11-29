f6b89b0ca9f311f4958e35273dfb24a82cae0f06
Gavin Joyce <gavinjoyce@gmail.com>
2016-11-29 15:17:45 +0000

    2.10 error demonstration

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,9 +1,31 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+let MyMixin = Ember.Mixin.create({
+  talk() {
+    console.log('GJ: MyMixin talking');
+    this._super(...arguments);
+  }
+});
+
+let MyObject = Ember.Object.extend(MyMixin, {
+  talk() {
+    console.log('GJ: MyObject talking');
+    this._super(...arguments);
+  }
+});
+
+let myInstance = MyObject.create();
+
+export default Ember.Route.extend(MyMixin, {
   actions: {
-    sayHello(name) {
-      alert(`Hi ${name}`);
+    sayHello() {
+      myInstance.talk(); //this works
+      myInstance['talk'](); //this works
+
+      let fn = myInstance['talk'];
+       //this fails with `Uncaught TypeError: Cannot read property '_super' of undefined`
+       //in Ember 2.10.0. It works in Ember 2.9.0
+      fn();
     }
   }
 });
